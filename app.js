203afa7813043293cdf8f31e4567310dfcb740c1
denniztr <denniztr@gmail.com>
2023-07-01 15:03:06 +0300

    выбор уровня

--- app.js
+++ app.js
@@ -7,17 +7,44 @@ function createLevelSelection () {
     levelSelectionTitle.classList.add('level-selection-title')
     levelSelectionBox.append(levelSelectionTitle);
 
-    const levelListBox = document.createElement('div');
-    levelListBox.classList.add('level-list-box');
+    const levelListBox = document.createElement('form');
+    levelListBox.setAttribute('id', 'level-form')
     levelSelectionBox.append(levelListBox);
-    levelListBox.innerHTML = `  <div class="level-box">1</div>
-                                <div class="level-box">2</div>
-                                <div class="level-box">3</div>  `;
+    levelListBox.innerHTML = `  <div class="level-selection-container">
+                                    <button type="button" value="1" class="level-button">1</button>
+                                    <button type="button" value="2" class="level-button">2</button>
+                                    <button type="button" value="3" class="level-button">3</button>
+                                </div>
+                                <div class="start-the-game-container">
+                                    <button type="submit" class="start-game-button">Старт</button>
+                                </div>  `;
 
-    const startGameButton = document.createElement('button');
-    startGameButton.classList.add('start-game-button');
-    startGameButton.innerHTML = 'Старт'
-    levelSelectionBox.append(startGameButton);
+    const levelSelectionWarning= document.createElement('p');
+    levelSelectionWarning.classList.add('level-selection-warning');
+    levelSelectionBox.append(levelSelectionWarning);
+
+    const levelButtons = document.querySelectorAll('.level-button');
+    levelButtons.forEach(button => {
+      button.addEventListener('click', function() {
+        levelButtons.forEach(btn => {
+          btn.classList.remove('selected');
+        });
+        this.classList.add('selected');
+      });
+    });
+
+    document.getElementById('level-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const selectedLevel = document.querySelector('.level-button.selected');
+        if (selectedLevel) {
+            const levelValue = selectedLevel.value;
+            console.log(`Начало игры с уровнем ${levelValue}`);
+        } else {
+            levelSelectionWarning.textContent = 'выберите уровень чтобы начать игру';
+            setTimeout(function() {
+                levelSelectionWarning.textContent = '';
+            }, 2000)
+        }
+    })
 }
 createLevelSelection()
-
